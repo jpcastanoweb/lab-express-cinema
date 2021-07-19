9462290d7a0a0dfea7de49c2c0877751a68c93f2
Juan Pablo Castano <jpcastanom@gmail.com>
2021-07-19 13:09:01 -0500

    all iterations except styling

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
+const Movie = require("./../models/Movie.model")
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"))
+router.get("/movies", (req, res, next) => {
+  Movie.find({})
+    .then((moviesFound) => {
+      res.render("movies", {
+        movies: moviesFound,
+      })
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+})
+router.get("/movies/:movieId", (req, res, next) => {
+  const { movieId } = req.params
 
-module.exports = router;
+  Movie.findById(movieId)
+    .then((movieFound) => {
+      console.log(movieFound)
+      res.render("singleMovie", {
+        movie: movieFound,
+      })
+    })
+    .catch((e) => console.log(e))
+})
+
+module.exports = router
